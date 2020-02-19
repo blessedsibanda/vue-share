b80a4d48d4cef88336fe58191cde08c77b1bfbc1
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2020-02-19 07:02:14 +0200

    created horizontal navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,117 +1,62 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
+    <v-app-bar fixed app color="primary">
+      <v-app-bar-nav-icon />
+      <v-toolbar-title class="hidden-sm-and-down">
+        <nuxt-link to="/" tag="span" style="cursor:pointer">VueShare</nuxt-link>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <!-- Search input-->
+      <v-text-field flex placeholder="Search posts" color="accent" single-line hide-details>
+        <v-icon color="black" slot="prepend">mdi-magnify</v-icon>
+      </v-text-field>
+
+      <v-spacer></v-spacer>
+
+      <!-- Horizontal Navbar links -->
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn
+          color="primary"
+          :to="item.link"
+          depressed
+          v-for="item in horizontalNavItems"
+          :key="item.title"
         >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+          <v-icon class="hidden-sm-and-down" left>{{item.icon}}</v-icon>
+          {{item.title}}
+        </v-btn>
+      </v-toolbar-items>
     </v-app-bar>
+
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
-      <span>&copy; 2019</span>
+
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2020</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      fixed: true
+    };
+  },
+  computed: {
+    horizontalNavItems() {
+      return [
+        { icon: "mdi-comment", title: "Posts", link: "/posts" },
+        { icon: "mdi-lock-open", title: "Sign In", link: "/signin" },
+        { icon: "mdi-account-plus", title: "Sign Up", link: "/signup" }
+      ];
     }
   }
-}
+};
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,87 +1,89 @@
-import colors from 'vuetify/es5/util/colors'
+import colors from 'vuetify/es5/util/colors';
 
 export default {
-  mode: 'universal',
-  /*
+	mode: 'universal',
+	/*
   ** Headers of the page
   */
-  head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  /*
+	head: {
+		titleTemplate: '%s - ' + process.env.npm_package_name,
+		title: process.env.npm_package_name || '',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+		],
+		link: [ { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' } ]
+	},
+	/*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
-  /*
+	loading: { color: '#fff' },
+	/*
   ** Global CSS
   */
-  css: [
-  ],
-  /*
+	css: [],
+	/*
   ** Plugins to load before mounting the App
   */
-  plugins: [
-  ],
-  /*
+	plugins: [],
+	/*
   ** Nuxt.js dev-modules
   */
-  buildModules: [
-    '@nuxtjs/vuetify',
-  ],
-  /*
+	buildModules: [ '@nuxtjs/vuetify' ],
+	/*
   ** Nuxt.js modules
   */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
-    // Doc: https://github.com/nuxt-community/dotenv-module
-    '@nuxtjs/dotenv',
-  ],
-  /*
+	modules: [
+		// Doc: https://axios.nuxtjs.org/usage
+		'@nuxtjs/axios',
+		'@nuxtjs/pwa',
+		// Doc: https://github.com/nuxt-community/dotenv-module
+		'@nuxtjs/dotenv'
+	],
+	/*
   ** Axios module configuration
   ** See https://axios.nuxtjs.org/options
   */
-  axios: {
-  },
-  /*
+	axios: {},
+	/*
   ** vuetify module configuration
   ** https://github.com/nuxt-community/vuetify-module
   */
-  vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
-  },
-  /*
+	vuetify: {
+		customVariables: [ '~/assets/variables.scss' ],
+		theme: {
+			// dark: true,
+			light: true,
+			themes: {
+				dark: {
+					primary: colors.red.lighten1,
+					accent: colors.teal.darken1,
+					secondary: colors.teal.lighten3,
+					info: colors.blue.accent3,
+					warning: colors.amber.base,
+					error: colors.red.accent4,
+					success: colors.green.darken4
+				},
+				light: {
+					primary: colors.indigo.darken1,
+					accent: colors.teal.darken1,
+					secondary: colors.teal.lighten3,
+					info: colors.blue.accent3,
+					warning: colors.amber.base,
+					error: colors.red.accent4,
+					success: colors.green.darken4
+				}
+			}
+		}
+	},
+	/*
   ** Build configuration
   */
-  build: {
-    /*
+	build: {
+		/*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
-    }
-  }
-}
+		extend(config, ctx) {}
+	}
+};
