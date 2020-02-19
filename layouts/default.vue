0108d40df32523c196143f9494aeb4cfab3e6983
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2020-02-19 08:16:38 +0200

    fix side nav bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,73 +1,49 @@
 <template>
   <v-app dark>
-    <v-app-bar fixed app color="primary">
-      <!-- Side navbar -->
-      <v-navigation-drawer app temporary fixed v-model="sideNav">
-        <v-toolbar color="secondary" dark flat>
-          <v-app-bar-nav-icon @click="toggleSideNav" />
-          <nuxt-link to="/" tag="span" style="cursor: pointer">
-            <h1 class="title pl-3">VueShare</h1>
-          </nuxt-link>
-        </v-toolbar>
+    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
+      <v-list-item @click="drawer = !drawer" tag="span" to="/" router exact>
+        <v-list-item-icon>
+          <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+        </v-list-item-icon>
 
-        <v-divider></v-divider>
-        <!-- Side Navbar links -->
-        <v-list>
-          <v-list-item :to="item.link" v-for="(item,i) in sideNavItems" :key="i">
-            <v-list-tile>
-              <v-list-tile-action>
-                <v-icon>{{item.icon}}</v-icon>
-              </v-list-tile-action>
-              <v-list-tile-content>{{item.title}}</v-list-tile-content>
-            </v-list-tile>
-          </v-list-item>
-        </v-list>
+        <v-list-item-content>
+          <v-list-item-title>
+            <h1 class="title">{{title}}</h1>
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
 
-        <v-divider></v-divider>
-        <v-list>
-          <v-subheader>REPORTS</v-subheader>
-          <v-list-item-group color="primary">
-            <v-list-item v-for="(item, i) in sideNavItems" :key="i">
-              <v-list-item-icon>
-                <v-icon v-text="item.icon"></v-icon>
-              </v-list-item-icon>
-              <v-list-item-content>
-                <v-list-item-title v-text="item.title"></v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>
-      </v-navigation-drawer>
+      <v-divider></v-divider>
+      <v-list>
+        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="clipped" fixed app color="primary">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
 
-      <v-app-bar-nav-icon @click="toggleSideNav" />
-      <v-toolbar-title class="hidden-sm-and-down">
-        <nuxt-link to="/" tag="span" style="cursor:pointer">VueShare</nuxt-link>
-      </v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <!-- Search input-->
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
       <v-text-field flex placeholder="Search posts" color="accent" single-line hide-details>
-        <v-icon color="black" slot="prepend">mdi-magnify</v-icon>
+        <v-icon slot="prepend">mdi-magnify</v-icon>
       </v-text-field>
 
       <v-spacer></v-spacer>
 
       <!-- Horizontal Navbar links -->
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn
-          color="primary"
-          :to="item.link"
-          depressed
-          v-for="item in horizontalNavItems"
-          :key="item.title"
-        >
+        <v-btn color="primary" :to="item.to" depressed v-for="item in items" :key="item.title">
           <v-icon class="hidden-sm-and-down" left>{{item.icon}}</v-icon>
           {{item.title}}
         </v-btn>
       </v-toolbar-items>
     </v-app-bar>
-
     <v-content>
       <v-container>
         <nuxt />
@@ -75,7 +51,7 @@
     </v-content>
 
     <v-footer :fixed="fixed" app>
-      <span>&copy; 2020</span>
+      <span>&copy; 2019</span>
     </v-footer>
   </v-app>
 </template>
@@ -84,30 +60,29 @@
 export default {
   data() {
     return {
-      fixed: true,
-      sideNav: false
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: "mdi-comment",
+          title: "Posts",
+          to: "/posts"
+        },
+        {
+          icon: "mdi-lock-open",
+          title: "Sign In",
+          to: "/signin"
+        },
+        {
+          icon: "mdi-account-plus",
+          title: "Sign Up",
+          to: "/signup"
+        }
+      ],
+      miniVariant: false,
+      title: "VueShare"
     };
-  },
-  computed: {
-    horizontalNavItems() {
-      return [
-        { icon: "mdi-comment", title: "Posts", link: "/posts" },
-        { icon: "mdi-lock-open", title: "Sign In", link: "/signin" },
-        { icon: "mdi-account-plus", title: "Sign Up", link: "/signup" }
-      ];
-    },
-    sideNavItems() {
-      return [
-        { icon: "mdi-comment", title: "Posts", link: "/posts" },
-        { icon: "mdi-lock-open", title: "Sign In", link: "/signin" },
-        { icon: "mdi-account-plus", title: "Sign Up", link: "/signup" }
-      ];
-    }
-  },
-  methods: {
-    toggleSideNav() {
-      this.sideNav = !this.sideNav;
-    }
   }
 };
 </script>
